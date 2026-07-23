bdf6933be17846841c742d2d75386b0bad351fd6
Jade Panilagan <jadepanilagan52@gmail.com>
2026-07-23 12:21:33 +0800

    Fix build script for Vercel build environment

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py seed_skills
-python manage.py createsuperuser --noinput || true
